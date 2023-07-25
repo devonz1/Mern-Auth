d09845942abcd99f1b57311fea8399c741162bb2
devon gibson <devongibs2@gmail.com>
2023-07-24 18:02:57 -0700

    updates to the authroutes file

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const cors = require ('cors');
-const { test } = require ('../controllers/authController')
+const cors = require('cors');
+const { test } = require('../controllers/authController')
 
 
 
@@ -15,7 +15,9 @@ router.use(
 )
 
 router.get('/', test)
+router.post('/register') /// This will allow the  user to register on our app and after that we will
+// be sending the post request to our endpoint of register which will add the user registration information to our database.
 
 module.exports = router
 
- 
+
