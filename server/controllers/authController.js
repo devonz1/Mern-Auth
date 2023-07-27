be9edd8daa41950d983ffef4ad5d3b4b477bda6a
devon gibson <devongibs2@gmail.com>
2023-07-26 17:31:39 -0700

    updates

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -4,7 +4,21 @@ const test = (req, res) =>{
 
 
 const registerUser = (req, res) =>{
-    
+    try {
+    const {name, email , password} = req.body;
+     // check if name was entered
+     if(!name){
+        return res.json({
+            error: 'name is required'
+     })
+     };
+     // check is password good
+     if(!password || password.length < 6 ){
+        return res.json()
+     }
+    } catch {error}{
+
+    }
 }
 
 module.exports = {
