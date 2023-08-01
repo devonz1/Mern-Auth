9db53e738d5b83bd0467413d9ad144cc75a0c4ed
devon gibson <devongibs2@gmail.com>
2023-07-31 17:20:40 -0700

    updates

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const {mongoose} =require('mongoose')
 const app = express();
 
 //database connection
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGOS_URL)
 .then(()=> console.log ('Database Connected'))
 .catch((err) => console.log ('Database not connected', err))
 
