f534d0f3797f79e92cdc0cced1c83613398fc32c
devon gibson <devongibs2@gmail.com>
2023-09-19 23:59:36 -0700

    updates

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -83,7 +83,9 @@ const loginUser = async (req, res) =>{
 const getProfile = (req, res) =>{
 const {token} = req.cookies
 if (token){
-  jwt.verify(token,process.env.JWT_SECRET,)
+  jwt.verify(token,process.env.JWT_SECRET,{},(err,user)=>{
+    
+  })
 }
 }
 
