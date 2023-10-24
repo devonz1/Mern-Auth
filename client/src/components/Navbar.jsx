b03d3b173d924446d907b4ee642cb769364c72f8
devon gibson <devongibs2@gmail.com>
2023-10-23 23:55:45 -0700

    updates

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,11 +3,23 @@
  
  export default function Navbar() {
    return (
-     <nav>
+    
+      
+      
+        <nav>
+
+       <li> 
       <Link to= '/'>Home</Link>
+      </li>
+      <li>
       <Link to= '/register'>Register</Link>
+       </li>
+       <li> 
       <Link to= '/login'>Login</Link>
-     </nav>
+      </li>
+      
+      </nav>
+      
    )
  }
  
